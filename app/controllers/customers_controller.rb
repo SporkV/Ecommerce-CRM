a2b8035e5e6c8d1f94be1d8e4eb92fa2a7c007ee
Wally Glutton <stungeye@gmail.com>
2012-11-15 14:50:14 -0600

    Changed the class of the menu. Modified the customers#destroy such that it redirects to the finder path. Also changed all links in the customers index (in the customers views) to the finder index.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -76,7 +76,7 @@ class CustomersController < ApplicationController
     @customer.destroy
 
     respond_to do |format|
-      format.html { redirect_to customers_url }
+      format.html { redirect_to finder_url }
       format.json { head :no_content }
     end
   end
